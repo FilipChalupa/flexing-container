029051f00472b94981afac59ab42e894b6c474bc
Filip Chalupa <chalupa.filip@gmail.com>
2022-09-16 18:27:18 +0200

    Improve Eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,34 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'plugin:react/recommended',
+		'standard-with-typescript',
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+		project: ['tsconfig.json'],
+	},
+	plugins: ['react', 'react-hooks', 'prettier', '@typescript-eslint'],
+	rules: {
+		'prettier/prettier': ['error'],
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'comma-dangle': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
 }
